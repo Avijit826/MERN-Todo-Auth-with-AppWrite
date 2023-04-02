3541c4912713868720bb48d3bd6fce4511588dd6
Avijit Das <avijitdas826@gmail.com>
2023-04-02 22:01:45 +0530

    📦 chore(frontend): add appwrite dependency to package.json 🔧 chore(frontend): add appwrite configuration file 🔧 chore(frontend): add appwrite client configuration 🔧 chore(frontend): add appwrite account configuration 🔧 chore(frontend): add appwrite project configuration 🔧 chore(frontend): add appwrite endpoint configuration 🔧 chore(frontend): add appwrite context configuration 🔧 chore(frontend): add appwrite ID configuration 🔧 chore(frontend): update .gitignore to exclude appwrite configuration file 🔧 chore(frontend): update .gitignore to exclude appwrite client configuration 🔧 chore(frontend): update .gitignore to exclude appwrite account configuration 🔧 chore(frontend): update .gitignore to exclude appwrite project configuration 🔧 chore(frontend): update .gitignore to exclude appwrite endpoint configuration

---
+++ frontend/src/config/config.js
@@ -0,0 +1,11 @@
+import { Client, Account, ID } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('http://localhost/v1')
+    .setProject('64286f0c712128230c27');
+
+const account = new Account(client);
+
+exports = { account, ID }
